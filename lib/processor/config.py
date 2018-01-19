7472eea8a84cf24ed51468bc2fc6be69209545a5
Markus Baur <markusb@selfnet.de>
2018-01-19 16:02:49 +0100

    Remove old python stuff

--- lib/processor/config.py
+++
@@ -1,7 +0,0 @@
-
-extensions = ['c', 'h', 'cpp', 'hpp']
-extensions_ignore = ['gch']
-main_regexes = [
-    'int[\n\r\s]+main[\n\r\s]*\([\n\r\s]*(void)?[\n\r\s]*\)',
-    'int[\n\r\s]+main[\n\r\s]*\([\n\r\s]*int[\n\r\s]+\w+[\n\r\s]*,[\n\r\s]*char[^\)]*\)',
-]
